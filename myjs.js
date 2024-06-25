506e31eb24deb14855ef594ddfe5007faafee260
sugibuchiyuki <167832771+sugibuchiyuki@users.noreply.github.com>
2024-06-25 16:44:10 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,10 +5,10 @@ alert(Math.sqrt2)+(Math.sqrt3);
 alert(1/0);
 alert(0/0);
 alert(2+3);
-alert("2")+("3");
-alert(Number("2"))+(Number("3"));
-alert(("2")+3);
-alert(Number("2")+3);
+alert("2"+"3");
+alert(Number"2"+(Number"3");
+alert("2"+3);
+alert(Number"2"+3);
 
 
 
