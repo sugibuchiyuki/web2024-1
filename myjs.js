ee96dcf8b79be8252d29ae8b5a901f80eb7dea41
sugibuchiyuki <167832771+sugibuchiyuki@users.noreply.github.com>
2024-06-25 16:33:13 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,5 +1,14 @@
 alert("Hallo,yuki!");
-
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert(Math.sqrt(2))+(Math.sqrt(3));
+alert(1/0);
+alert(0/0);
+alert(2+3);
+alert("2")+("3");
+alert(Number("2"))+(Number("3"));
+alert(("2")+3);
+alert(Number("2")+3);
 
 
 
